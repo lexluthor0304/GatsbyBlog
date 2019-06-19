120eacfee45c03469fb8f6d44dbaa32698b75c9a
lexluthor0304 <lexluthor0304@gmail.com>
2019-06-19 23:35:38 +0900

    続く

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 const AboutPage = () => {
     return(
         <div>
             <h1>About</h1>
             <h2>About me</h2>
+            <p>want to contact me?<Link to="/contact">click here</Link></p>
         </div>
     )
 }
