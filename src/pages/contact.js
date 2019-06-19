11f7b538ddd426d84d35d40d9c08ce2a68f3dd4f
lexluthor0304 <lexluthor0304@gmail.com>
2019-06-19 23:52:34 +0900

    必要なモジュールを追加

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,13 +1,17 @@
-import React from 'react'
-import Footer from '../component/footer'
+import React from "react";
+import Footer from "../component/footer";
 
 const ContactPage = () => {
-    return(
-        <div>
-            <h1>Contact</h1>
-            <h2>Contact me via <a href="https://github.com/lexluthor0304">Github : lexluthor </a></h2>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Contact</h1>
+      <p>
+        Contact me via{" "}
+        <a href="https://github.com/lexluthor0304">Github : lexluthor</a>
+        <Footer />
+      </p>
+    </div>
+  )
 }
 
-export default ContactPage
+export default ContactPage;
