f6ea2d7214d3ec0c3681337d6e45d67f1ed6b29e
lexluthor0304 <lexluthor0304@gmail.com>
2019-06-19 23:21:26 +0900

    プロジェクト初期化
    index, blog, about, contactページ追加

---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ContactPage = () => {
+    return(
+        <div>
+            <h1>Contact</h1>
+            <h2>Contact me via <a href="https://github.com/lexluthor0304">Github : lexluthor </a></h2>
+        </div>
+    )
+}
+
+export default ContactPage
---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { Link } from 'gatsby'
+
+const IndexPage = () => {
+    return(
+        <div>
+            <h1>Hello</h1>
+            <h2>I am Sai</h2>
+            <p>Need a developer <a href="/contact">contact me</a></p>
+            <p>Need a developer <Link to="/contact">Contact me. </Link></p>
+        </div>
+    )
+}
+
+export default IndexPage
