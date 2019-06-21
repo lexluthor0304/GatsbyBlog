100b7f9c0d2919b8e07d2fc13a1b1c09ccbde3cd
lexluthor0304 <lexluthor0304@gmail.com>
2019-06-21 20:36:34 +0900

    env-cmdを追加、GRAPHQLを美化

--- src/component/footer.js
+++ src/component/footer.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { graphql, useStaticQuery } from "gatsby"
 
 const Footer = () => {
+    const date = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          author
+        }
+      }
+    }
+  `)
     return(
         <footer>
-            <p>Created by lexluthor, copy@2019</p>
+            <p>Created by {date.site.siteMetadata.author}, ©2019</p>
         </footer>
     )
 }
