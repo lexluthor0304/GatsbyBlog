5651dd4ee2e32c9ecac384c25cd3f22a7869b944
lexluthor0304 <lexluthor0304@gmail.com>
2019-06-21 16:08:15 +0900

    動的タイトル設定,スタイルシート更新

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,8 @@
 module.exports = {
+    siteMetadata: {
+        title: 'Full-Stack Engineer Lex',
+        author: 'Lex Luthor'
+    },
     plugins: [
         'gatsby-plugin-sass'
     ]
--- src/component/header.js
+++ src/component/header.js
@@ -1,29 +1,63 @@
-import React from "react";
-import { Link } from "gatsby";
-// import './header.module.scss'
-import headerStyles from './header.module.scss'
+import React from "react"
+import { Link, graphql, useStaticQuery } from "gatsby"
+
+import headerStyles from "./header.module.scss"
 
 const Header = () => {
+  const date = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
   return (
     <header className={headerStyles.header}>
       <h1>
         <Link className={headerStyles.title} to="/">
-        Lex's Blog build with Gatsby
+          {date.site.siteMetadata.title}
         </Link>
       </h1>
       <nav>
         <ul className={headerStyles.navList}>
           <li>
-            <Link className={headerStyles.navItem} to="/">Home</Link>
+            <Link
+              className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}
+              to="/"
+            >
+              Home
+            </Link>
           </li>
           <li>
-            <Link className={headerStyles.navItem} to="/blog">Blog</Link>
+            <Link
+              className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}
+              to="/blog"
+            >
+              Blog
+            </Link>
           </li>
           <li>
-            <Link className={headerStyles.navItem} to="about">About</Link>
+            <Link
+              className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}
+              to="about"
+            >
+              About
+            </Link>
           </li>
           <li>
-            <Link className={headerStyles.navItem} to="/contact">Contact</Link>
+            <Link
+              className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}
+              to="/contact"
+            >
+              Contact
+            </Link>
           </li>
         </ul>
       </nav>
