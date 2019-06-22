27766c9a360d570742fdec4c71ee146410149d6b
lexluthor0304 <lexluthor0304@gmail.com>
2019-06-22 21:33:20 +0900

    動的投稿機能

--- src/posts/gatsby.md
+++ src/posts/gatsby.md
@@ -1,5 +1,5 @@
 ---
-title: "test post"
+title: "テスト用投稿"
 date: "2019-06-21"
 ---
 
---
+++ src/templates/blog.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../component/layout'
+
+const Blog = () => {
+    return (
+        <Layout>
+            これはブログのテンプレート
+        </Layout>
+    )
+}
+
+export default Blog
