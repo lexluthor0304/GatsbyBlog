f9514b6160f7475aa0271de191645b25bab87e8d
lexluthor0304 <lexluthor0304@gmail.com>
2019-06-23 13:19:00 +0900

    投稿本文表示機能追加

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,10 +1,25 @@
 import React from 'react'
+import { graphql } from "gatsby"
 import Layout from '../component/layout'
 
-const Blog = () => {
+export const query = graphql`
+    query ($slug: String!) {
+        markdownRemark ( fields: { slug: { eq: $slug } }) {
+            frontmatter{
+                title
+                date
+            }
+            html
+        }
+    }
+`
+
+const Blog = (props) => {
     return (
         <Layout>
-            これはブログのテンプレート
+            <h1>{props.data.markdownRemark.frontmatter.title}</h1>
+            <p>{props.data.markdownRemark.frontmatter.date}</p>
+            <div dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}></div>
         </Layout>
     )
 }
